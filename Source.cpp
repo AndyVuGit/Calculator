73d323eabcd8bdad07e5198f0c4dbeeba77cd0ad
Andy Vu <phucvu95@gmail.com>
2018-01-10 18:03:58 -0800

    Evaluation Update
    Add  more goals to things to do.  Moved the evaluations over to their own seperate methods and added a complex evaluation method.

--- Source.cpp
+++ Source.cpp
@@ -8,21 +8,42 @@ using namespace std;
 /* Things to do
 1. set up string parsing
 2. set up basic functions
-3. move functions to a class
+3. move evaulations to a class or functions
 4. add more complex functions
+4b.change evaluations of the functions into subfunctions using recursion
 5. Made a graphic UI for calculator
 */
 
+//--Declaration of Funcs--
+void evaluation(string input);
+void complexEval(string input);
+
+//--
 
 int main()
 {
 	string input;
 
-
-	//1 + 2
 	cout << "Enter your equation" << endl;
 	getline(cin, input);
 
+	if (!isdigit(input[0]))
+	{
+		complexEval(input);
+	}
+	else
+	{
+		evaluation(input);
+	}
+
+	cout << endl << endl;
+	system("Pause");
+	return 0;
+}
+
+//Function for simple mathematical evaluations
+void evaluation(string input)
+{
 	istringstream iss(input);
 	//basic non complex interpretation 
 	//e.g. A + B
@@ -52,13 +73,46 @@ int main()
 	{
 		cout << firstNum / secondNum;
 	}
+	else if (operation == "%")
+	{
+		cout << firstNum % secondNum;
+	}
 	else
 	{
 		cerr << "Inappropriate operation" << endl;
 	}
+}
 
+//Function for more complex evaluation like sin, cos, tan, etc
+void complexEval(string input)
+{
+	//Get a substring of the first 3 characters to determine the evaluation needed
+	string subInput = input.substr(0, 3);
 
-	cout << endl << endl;
-	system("Pause");
-	return 0;
+	int number = input[4] - '0';
+
+	if (subInput == "tan")
+	{
+		cout << tan(number) << endl;
+	}
+	else if (subInput == "cos")
+	{
+		cout << cos(number) << endl;
+	}
+	else if (subInput == "sin")
+	{
+		cout << sin(number) << endl;
+	}
+	else if (subInput == "log")
+	{
+		cout << log(number) << endl;
+	}
+	else if (subInput == "EXP")
+	{
+		cout << exp(number) << endl;
+	}
+	else
+	{
+		cerr << "Inappropriate operation" << endl;
+	}
 }
